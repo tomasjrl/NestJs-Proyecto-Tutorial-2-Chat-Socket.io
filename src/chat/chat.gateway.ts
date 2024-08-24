d9f014414e633f10a9d430eecc61b11e1427543a
tomasjrl <tstabilini@gmail.com>
2024-08-24 19:17:10 -0300

    cliente conectado/desconectado

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,22 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnModuleInit } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnModuleInit {
+  @WebSocketServer()
+  public server: Server;
+
   constructor(private readonly chatService: ChatService) {}
+
+  onModuleInit() {
+    this.server.on('connection', (socket: Socket) => {
+      console.log('Cliente conectado:', socket.id);
+
+      socket.on('disconnect', () => {
+        console.log('Cliente desconectado:', socket.id);
+      });
+    });
+  }
 }
